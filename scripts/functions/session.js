7f9f1a618dfcc13a9397374a5e63d4be263b605e
lwdxoi <lwdxoi@proton.me>
2024-12-03 23:39:04 -0300

    add output on console

--- scripts/functions/session.js
+++ scripts/functions/session.js
@@ -2,7 +2,10 @@ async function login() {
   const baseApiUrl = document.querySelector("[name=baseApiUrl]").value
   const apiKey = document.querySelector("[name=apiKey]").value
 
+  console.log('session#login:5', baseApiUrl, apiKey)
+
   window.LwdOneApi = new SupabaseApi(baseApiUrl, apiKey)
+  console.log('session#login:8', 'LwdOneApi', new SupabaseApi(baseApiUrl, apiKey))
 }
 async function logout() {
   localStorage.clear();
